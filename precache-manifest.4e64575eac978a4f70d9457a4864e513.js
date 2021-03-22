6c265fa6ffaad59352ab64f2ea2195f75cc7d42c
Joushwa Shahzad <joushwashahzad1@gmail.com>
2021-03-22 20:25:42 +0500

    Updates

---
+++ precache-manifest.4e64575eac978a4f70d9457a4864e513.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65006c721197e639caf26b2b13165342",
+    "url": "/Amazon-clone-By-JoushwaS/index.html"
+  },
+  {
+    "revision": "bf842eab600c5dc8d173",
+    "url": "/Amazon-clone-By-JoushwaS/static/css/2.65a8c18f.chunk.css"
+  },
+  {
+    "revision": "69c2a538e69d2aa2eeb9",
+    "url": "/Amazon-clone-By-JoushwaS/static/css/main.db2c5887.chunk.css"
+  },
+  {
+    "revision": "bf842eab600c5dc8d173",
+    "url": "/Amazon-clone-By-JoushwaS/static/js/2.f21485dc.chunk.js"
+  },
+  {
+    "revision": "ca946b70e3a2d8086192f3f96542523f",
+    "url": "/Amazon-clone-By-JoushwaS/static/js/2.f21485dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69c2a538e69d2aa2eeb9",
+    "url": "/Amazon-clone-By-JoushwaS/static/js/main.517275f3.chunk.js"
+  },
+  {
+    "revision": "ba65a95f1f97b98a2871",
+    "url": "/Amazon-clone-By-JoushwaS/static/js/runtime-main.3ccee3bb.js"
+  }
+]);
